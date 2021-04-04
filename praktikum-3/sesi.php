d640b02ede3c2723fb61a6cd3004e57c64e92b21
Akbar Satrio <akbarsatrio@outlook.co.id>
2021-04-04 20:00:42 +0700

    add

--- praktikum-3/sesi.php
+++ praktikum-3/sesi.php
@@ -1,12 +1,1 @@
-<?php
 
-session_start();
-if($_SESSION['result'] == null) {
-  $_SESSION = ['result'];
-}
-$_SESSION['result'][] = [
-  'ya' => 'yoyo',
-  'tai' => 'anjing'
-];
-// unset($_SESSION['result']);
-echo json_encode($_SESSION['result']);
